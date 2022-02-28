b860e7bc4e9bd104718e6ace63696f2d983d2efe
unknown <brunojsfla@gmail.com>
2022-02-28 12:32:59 -0300

    feat: insert icons

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   Container,
   Header,
@@ -8,6 +9,7 @@ import {
   User,
   UserGreeting,
   UserName,
+  Icon
 } from "./styles";
 
 export function Dashboard() {
@@ -26,6 +28,7 @@ export function Dashboard() {
               <UserName>Bruno!</UserName>
             </User>
           </UserInfo>
+          <Icon name="power"/>
         </UserWraper>
       </Header>
     </Container>
